e7efc683d7d32a5c029e1f1522a5c9e8bb9e84db
Pieter Stevens <pieter.stevens@icloud.com>
2024-11-13 16:29:59 +0100

    projectile as fireball

--- projectile.gd
+++ projectile.gd
@@ -1,11 +1,12 @@
 extends CharacterBody2D
 
-@export var speed := 350.0
+@export var speed := 300.0
 
 func _ready() -> void:
 	velocity *= speed
 
 func _physics_process(delta: float) -> void:
+	velocity *= 1.01
 	move_and_slide()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
