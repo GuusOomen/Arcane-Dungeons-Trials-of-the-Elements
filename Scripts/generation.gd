0c01d4501f6c5c77db23270c08b1974ba68624ce
Pieter Stevens <pieter.stevens@icloud.com>
2025-01-09 15:38:43 +0100

    fire room start; other room visual fixes

--- Scripts/generation.gd
+++ Scripts/generation.gd
@@ -4,14 +4,19 @@ extends Node2D
 @export var min_depth := 3
 @export var starting_room: PackedScene
 @export var room_pool: Array[PackedScene]
+@export var tree_pool: Array[PackedScene]
+@export var required_rooms: Array[PackedScene]
 @export var ending_pool: Array[PackedScene]
 
-func generate_(prev_room: Node2D, prev_teleporter: Area2D, prev_room_scene: PackedScene, depth: int) -> Array[Node2D]:
+func generate_(prev_room: Node2D, prev_teleporter: Area2D, prev_room_scene: PackedScene, depth: int, path: int) -> Array[Node2D]:
 	var room_scene: PackedScene
 	if depth == max_depth:
 		room_scene = ending_pool.pick_random()
 	elif depth < min_depth:
-		room_scene = room_pool.pick_random()
+		if 0 != path:
+			room_scene = (room_pool + tree_pool).pick_random()
+		else:
+			room_scene = room_pool.pick_random()
 	else:
 		room_scene = (room_pool + ending_pool).pick_random()
 	var room: Node2D = room_scene.instantiate()
@@ -27,7 +32,7 @@ func generate_(prev_room: Node2D, prev_teleporter: Area2D, prev_room_scene: Pack
 				child.teleporter = prev_teleporter
 				teleporter = child
 				continue
-			var result := generate_(room, child, starting_room, depth + 1)
+			var result := generate_(room, child, starting_room, depth + 1, path)
 			child.room = result[0]
 			child.teleporter = result[1]
 	return [room, teleporter]
@@ -35,11 +40,15 @@ func generate_(prev_room: Node2D, prev_teleporter: Area2D, prev_room_scene: Pack
 func generate() -> void:
 	var room: Node2D = starting_room.instantiate()
 	add_child(room)
-	for child in room.get_children():
+	var path: int
+	var children := room.get_children()
+	children.shuffle()
+	for child in children:
 			if child.is_in_group("Teleporter"):
-				var result := generate_(room, child, starting_room, 0)
+				var result := generate_(room, child, starting_room, 0, path)
 				child.room = result[0]
 				child.teleporter = result[1]
+				path += 1
 
 func _ready() -> void:
 	generate()
