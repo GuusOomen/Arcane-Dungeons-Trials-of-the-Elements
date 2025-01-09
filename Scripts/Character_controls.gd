5da674183f50fa0e771a75e8abc4e03d2f2a0a0d
Pieter Stevens <pieter.stevens@icloud.com>
2025-01-09 12:20:52 +0100

    minor fixes

--- Scripts/Character_controls.gd
+++ Scripts/Character_controls.gd
@@ -55,10 +55,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	# Handle movement input in four directions
-	var input_vector := Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	).normalized()
+	var input_vector := Input.get_vector("left", "right", "up", "down")
 	if input_vector != Vector2.ZERO:
 		last_input_vector = input_vector
 	# Determine the character's facing direction based on input
